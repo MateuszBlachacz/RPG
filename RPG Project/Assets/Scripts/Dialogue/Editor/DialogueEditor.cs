b59ebee6a258585bb53d809c53bf59f6ed21788a
MateuszBlachacz <55129562+MateuszBlachacz@users.noreply.github.com>
2021-02-08 18:16:01 +0100

    6. Opening An Asset Callback

--- RPG Project/Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
+++ RPG Project/Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 namespace RPG.Dialogue.Editor { 
@@ -11,6 +12,20 @@ namespace RPG.Dialogue.Editor {
         public static void ShowEditorWindow()
         {
             GetWindow(typeof(DialogueEditor),false, "Dialogue Editor");
+        }
+
+        [OnOpenAssetAttribute(1)]
+        public static bool OnOpenAsset(int instaceId, int line)
+        {
+            Dialogue dialogue = EditorUtility.InstanceIDToObject(instaceId) as Dialogue;
+            if (dialogue)
+            {
+                ShowEditorWindow();
+                return true;
+            }
+            
+
+            return false;
         }
     }
 
