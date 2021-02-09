6f4de8ac32e40dd6d317880a9a68bdd8fe37b0e2
MateuszBlachacz <55129562+MateuszBlachacz@users.noreply.github.com>
2021-02-09 11:51:26 +0100

    8 Callback Mechanisms

--- RPG Project/Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
+++ RPG Project/Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -8,13 +8,14 @@ namespace RPG.Dialogue.Editor {
 
     public class DialogueEditor : EditorWindow
     {
+        Dialogue selectedDialogue = null;
         [MenuItem("Window/Dialogue Editor")]
         public static void ShowEditorWindow()
         {
             GetWindow(typeof(DialogueEditor),false, "Dialogue Editor");
         }
 
-        [OnOpenAssetAttribute(1)]
+        [OnOpenAsset(1)]
         public static bool OnOpenAsset(int instaceId, int line)
         {
             Dialogue dialogue = EditorUtility.InstanceIDToObject(instaceId) as Dialogue;
@@ -27,11 +28,32 @@ namespace RPG.Dialogue.Editor {
             return false;
         }
 
+        private void OnEnable()
+        {
+            Selection.selectionChanged += OnSelectionChanged; 
+        }
+
+        private void OnSelectionChanged()
+        {
+            // selectedDialogue = Selection.activeObject as Dialogue;
+            Dialogue newDialogue = Selection.activeObject as Dialogue;
+            if (newDialogue) {
+                selectedDialogue = newDialogue;
+                Repaint();
+            }
+            Debug.Log("On selection change");
+            
+        }
         private void OnGUI()
         {
-            EditorGUILayout.LabelField("Hello, World");
-            EditorGUILayout.LabelField("Hello, World");
-            EditorGUILayout.LabelField("Hello, World");
+            if (null == selectedDialogue)
+            {
+                EditorGUILayout.LabelField("No Dialogue Selected.");
+            } 
+            else
+            {
+                EditorGUILayout.LabelField(selectedDialogue.name);
+            }
         }
     }
 
